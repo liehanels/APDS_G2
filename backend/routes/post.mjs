6f50a6824f614f434614e83c3687c7a9fb2ddb5b
Liehan Els <ST10085345@vcconnect.edu.za>
2024-08-14 11:51:44 +0200

    new version
    newest

--- backend/routes/post.mjs
+++ backend/routes/post.mjs
@@ -20,8 +20,36 @@ router.post("/upload", async (req, res) => {
     let result = await collection.insertOne(newDocument);
     res.send(result).status(204);
 });
-router.get("/default", async (req, res) => {
+
+router.patch("/:id", async (req, res) => {
+    const query = {_id: new ObjectId(req.params.id)};
+    const updates = {
+        $set: {
+            user: req.body.user,
+            content: req.body.content
+        }
+    };
     let collection = await db.collection("posts");
-    let results = await collection.find({}).toArray();
-    res.send(results).status(200);
+    let result = await collection.updateOne(query, updates);
+
+    res.send(result).status(200);
 });
+
+router.get("/:id", async (req, res) => {
+    let collection = await db.collection("posts");
+    let query = {_id: new ObjectId(req.params.id)};
+    let result = await collection.findOne(query);
+
+    if (!result) res.send("Not found").status(404);
+    else res.send(result).status(200);
+});
+
+router.delete("/:id", async (req, res) => {
+    const query = {_id: new ObjectId(req.params.id)};
+    const collection = db.collection("posts");
+    let result = await collection.deleteOne(query);
+
+    res.send(result).status(200);
+});
+
+export default router;
